de0a10324ad4f6052cd4b26f1df0212fdb57f274
Calvin Phung <calvinphung0@gmail.com>
2021-03-11 17:56:43 -0800

    add globe

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
+import Globe from 'react-globe.gl';
+import * as d3 from "d3";
 
 export default class App extends Component {
-  state = { username: null };
+  state = { places: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
+    fetch('../../datasets/ne_110m_populated_places_simple.geojson')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(data => this.setState({ places: data.features }));
   }
 
   render() {
-    const { username } = this.state;
+    const { places } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+          <Globe
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+            backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+
+            labelsData={places}
+            labelLat={d => d.properties.latitude}
+            labelLng={d => d.properties.longitude}
+            labelText={d => d.properties.name}
+            labelSize={d => Math.sqrt(d.properties.pop_max) * 4e-4}
+            labelDotRadius={d => Math.sqrt(d.properties.pop_max) * 4e-4}
+            labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+            labelResolution={2}
+            onLabelClick={() => alert("I am an alert box!")}
+          />, 
       </div>
     );
   }
