97259ceddedaea4698a3512732f2977602340c1a
Calvin Phung <calvinphung0@gmail.com>
2021-05-25 14:55:42 -0700

    change server route

--- src/server/index.js
+++ src/server/index.js
@@ -149,7 +149,7 @@ app.get('/api/keywords', async (req, res) => {
   }
 });
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
